9b21115038ad29b30dc76d6ffcfcd457910b22b7
AstroVulpix <astrovulpix.val@gmail.com>
2026-07-23 21:58:19 -0700

    cards

--- tzolkin-farm/hand_image.gd
+++ tzolkin-farm/hand_image.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 
 var time := 0.0
 var window_size = DisplayServer.screen_get_size()
-var mult = window_size.y * 0.00011
+var mult = window_size.y * 0.000
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	time += delta
